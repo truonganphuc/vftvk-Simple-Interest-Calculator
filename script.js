52296c8b1bd4282016779c1036384732a7a17626
Truong An Phuc <truonganphucbme@gmail.com>
2021-09-19 14:00:40 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principle = document.getElementById("principal").value;
     
+    if(principle == "" || principle <= 0)
+    {
+        alert("Enter a posivtive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principle * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    
+    document.getElementById("result").innerHTML = "If you deposit " + principle + ", <br> at an interest rate of " + rate + "%" + "<br> You will receive an amount of " + interest + ", <br> in the year " + year
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
